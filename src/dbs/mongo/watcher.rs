8cd38f442c89d8b76d0c3c94e1b1fc23adebf673
Keng <ssiwat.keng@gmail.com>
2025-07-11 22:45:39 +0700

    add retry logic to changestream

--- src/dbs/mongo/watcher.rs
+++ src/dbs/mongo/watcher.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use futures::StreamExt;
 use mongodb::{
     Collection,
@@ -5,6 +7,7 @@ use mongodb::{
     options::ChangeStreamOptions,
 };
 use serde::de::DeserializeOwned;
+use tokio::time::sleep;
 
 use crate::dbs::redis::{redis_get, redis_set};
 
@@ -19,19 +22,49 @@ where
     Fut: Future<Output = ()> + Send + 'static,
 {
     let redis_key = format!("changestream:resume:{}", coll.name());
-    let mut builder = coll.watch().with_options(options);
-    if let Some(token_str) = redis_get::<String>(&redis_key).await {
-        let token: ResumeToken = serde_json::from_str(&token_str)?;
-        builder = builder.resume_after(token);
-    }
-    let mut stream = builder.await?;
     tokio::spawn(async move {
-        while let Some(Ok(evt)) = stream.next().await {
-            let resume_token = evt.id.clone();
-            handler(evt).await;
-            let token_str =
-                serde_json::to_string(&resume_token).expect("Failed to serialize resume token");
-            redis_set(&redis_key, &token_str).await;
+        loop {
+            let mut builder = coll.watch().with_options(options.clone());
+            if let Some(token_str) = redis_get::<String>(&redis_key).await {
+                match serde_json::from_str::<ResumeToken>(&token_str) {
+                    Ok(token) => builder = builder.resume_after(token),
+                    Err(e) => {
+                        tracing::warn!(collection = coll.name(), error = %e, "invalid resume token")
+                    }
+                }
+            }
+
+            let mut stream = match builder.await {
+                Ok(stream) => stream,
+                Err(e) => {
+                    tracing::error!(collection = coll.name(), error = %e, "failed to start change stream");
+                    sleep(Duration::from_secs(5)).await;
+                    continue;
+                }
+            };
+
+            while let Some(evt_res) = stream.next().await {
+                match evt_res {
+                    Ok(evt) => {
+                        let resume_token = evt.id.clone();
+                        handler(evt).await;
+                        if let Ok(token_str) = serde_json::to_string(&resume_token) {
+                            redis_set(&redis_key, &token_str).await;
+                        } else {
+                            tracing::warn!(
+                                collection = coll.name(),
+                                "failed to serialize resume token"
+                            );
+                        }
+                    }
+                    Err(e) => {
+                        tracing::warn!(collection = coll.name(), error = %e, "change stream error, restarting");
+                        break;
+                    }
+                }
+            }
+
+            sleep(Duration::from_secs(5)).await;
         }
     });
     Ok(())
