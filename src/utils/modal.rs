144abf3b479177f77af46c829d9266d3218542b9
Keng <ssiwat.keng@gmail.com>
2025-07-19 15:28:15 +0700

    modal unit tests

--- src/utils/modal.rs
+++ src/utils/modal.rs
@@ -10,3 +10,46 @@ pub fn modal_value_of<'a>(data: &'a ModalInteractionData, id: &str) -> Option<&'
     }
     None
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use twilight_model::application::interaction::modal::{
+        ModalInteractionDataActionRow, ModalInteractionDataComponent,
+    };
+    use twilight_model::channel::message::component::ComponentType;
+
+    fn build_data() -> ModalInteractionData {
+        ModalInteractionData {
+            custom_id: "test".into(),
+            components: vec![
+                ModalInteractionDataActionRow {
+                    components: vec![ModalInteractionDataComponent {
+                        custom_id: "a".into(),
+                        kind: ComponentType::TextInput,
+                        value: Some("1".into()),
+                    }],
+                },
+                ModalInteractionDataActionRow {
+                    components: vec![ModalInteractionDataComponent {
+                        custom_id: "b".into(),
+                        kind: ComponentType::TextInput,
+                        value: None,
+                    }],
+                },
+            ],
+        }
+    }
+
+    #[test]
+    fn finds_value_for_id() {
+        let data = build_data();
+        assert_eq!(modal_value_of(&data, "a"), Some("1"));
+    }
+
+    #[test]
+    fn returns_none_when_absent() {
+        let data = build_data();
+        assert_eq!(modal_value_of(&data, "missing"), None);
+    }
+}
